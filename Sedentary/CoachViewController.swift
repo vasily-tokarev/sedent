dd0f7eb5d68e9cf04ee9e96a672170fbe07a2128
Vasily Tokarev <vasily.t@icloud.com>
2017-10-27 15:43:20 +0300

    Prevent screen sleep

--- Sedentary/CoachViewController.swift
+++ Sedentary/CoachViewController.swift
@@ -22,6 +22,8 @@ class CoachViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
 
+        UIApplication.shared.isIdleTimerDisabled = true // Disable it for other views?
+
         dateNotificationCreated = Date()
         coach.coachViewDelegate = self
         print("refreshing")
