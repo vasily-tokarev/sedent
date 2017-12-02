0038b9de38c2d7d6904ba0877e6d44e994048eab
Vasily Tokarev <vasily.t@icloud.com>
2017-12-02 15:11:01 +0300

    Changed deployment target (now 10.0) Test preparation

--- SedentTests/SedentTests.swift
+++ SedentTests/SedentTests.swift
@@ -10,20 +10,31 @@ import XCTest
 @testable import Sedent
 
 class SedentTests: XCTestCase {
+    var coach: Coach!
+    var exerciseIndex: Int!
     
     override func setUp() {
         super.setUp()
         // Put setup code here. This method is called before the invocation of each test method in the class.
+        
+        coach = Coach()
     }
     
     override func tearDown() {
+        exerciseIndex = nil
+        
         // Put teardown code here. This method is called after the invocation of each test method in the class.
         super.tearDown()
     }
     
+    func testCoachIsInitiated() {
+        exerciseIndex = coach.currentExerciseIndex
+        XCTAssertEqual(exerciseIndex, 0, "Exercise index is wrong")
+    }
+    
     func testExample() {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
+//         This is an example of a functional test case.
+//         Use XCTAssert and related functions to verify your tests produce the correct results.
     }
     
     func testPerformanceExample() {
--- SedentUITests/SedentUITests.swift
+++ SedentUITests/SedentUITests.swift
@@ -8,6 +8,72 @@
 
 import XCTest
 
+import XCTest
+import Foundation
+
+class Springboard {
+    
+    let springboard = XCUIApplication(bundleIdentifier: "com.apple.springboard")
+    let settings = XCUIApplication(bundleIdentifier: "com.apple.Preferences")
+    
+    
+    /**
+     Terminate and delete the app via springboard
+     */
+    func deleteMyApp() {
+        XCUIApplication().terminate()
+        
+        // Resolve the query for the springboard rather than launching it
+        springboard.activate()
+        
+        // Rotate back to Portrait, just to ensure repeatability here
+        XCUIDevice.shared.orientation = UIDeviceOrientation.portrait
+        // Sleep to let the device finish its rotation animation, if it needed rotating
+        sleep(2)
+        
+        // Force delete the app from the springboard
+        // Handle iOS 11 iPad 'duplication' of icons (one nested under "Home screen icons" and the other nested under "Multitasking Dock"
+        let icon = springboard.otherElements["Home screen icons"].scrollViews.otherElements.icons["YourAppName"]
+        if icon.exists {
+            let iconFrame = icon.frame
+            let springboardFrame = springboard.frame
+            icon.press(forDuration: 2.5)
+            
+            // Tap the little "X" button at approximately where it is. The X is not exposed directly
+            springboard.coordinate(withNormalizedOffset: CGVector(dx: ((iconFrame.minX + 3) / springboardFrame.maxX), dy:((iconFrame.minY + 3) / springboardFrame.maxY))).tap()
+            // Wait some time for the animation end
+            Thread.sleep(forTimeInterval: 0.5)
+            
+            //springboard.alerts.buttons["Delete"].firstMatch.tap()
+            springboard.buttons["Delete"].firstMatch.tap()
+            
+            // Press home once make the icons stop wiggling
+            XCUIDevice.shared.press(.home)
+            // Press home again to go to the first page of the springboard
+            XCUIDevice.shared.press(.home)
+            // Wait some time for the animation end
+            Thread.sleep(forTimeInterval: 0.5)
+            
+            // Handle iOS 11 iPad 'duplication' of icons (one nested under "Home screen icons" and the other nested under "Multitasking Dock"
+            let settingsIcon = springboard.otherElements["Home screen icons"].scrollViews.otherElements.icons["Settings"]
+            if settingsIcon.exists {
+                settingsIcon.tap()
+                settings.tables.staticTexts["General"].tap()
+                settings.tables.staticTexts["Reset"].tap()
+                settings.tables.staticTexts["Reset Location & Privacy"].tap()
+                // Handle iOS 11 iPad difference in error button text
+                if UIDevice.current.userInterfaceIdiom == .pad {
+                    settings.buttons["Reset"].tap()
+                }
+                else {
+                    settings.buttons["Reset Warnings"].tap()
+                }
+                settings.terminate()
+            }
+        }
+    }
+}
+
 class SedentUITests: XCTestCase {
         
     override func setUp() {
@@ -27,6 +93,10 @@ class SedentUITests: XCTestCase {
         // Put teardown code here. This method is called after the invocation of each test method in the class.
         super.tearDown()
     }
+    
+    func testNotificationInterval() {}
+    
+    func testWorkoutsReorder() {}
     
     func testExample() {
         // Use recording to get started writing UI tests.
